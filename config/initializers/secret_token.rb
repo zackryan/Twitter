4fb04e7d0cc61812f441a51384783befb67c5c3d
Zack Ryan <zackryan23@gmail.com>
2012-08-20 20:26:16 -0400

    rails new shitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shitter::Application.config.secret_token = '48682ca9c8f8468fd082e62eba9a8046c8d067a17af9bf2f07491181d06a2be3bcbbd6e007e96552dccac3a3bfd2361914af56816796b59649249b47832566a1'
